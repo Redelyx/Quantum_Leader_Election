e80aaf57475a8d16797c8182a8c86599adeeae3f
redelyx <alice.lv@live.it>
2023-11-10 14:55:41 +0100

    added support for multiple runs and medium execution time calculation

--- main_log_parallel.py
+++ main_log_parallel.py
@@ -143,27 +143,54 @@ def weak_coin_flip(p1, p2, coeff):
     return winner
 
 if __name__ == "__main__":
+    times = 1
+    mem_timings = []
+    players = [[],[]]
+
+    if len(sys.argv) == 1:
+        nParties = input("Number of parties:")
     if len(sys.argv) > 1:
         nParties = sys.argv[1]
-    else:
-        nParties = input("Number of parties:")
-    players = []
+    if len(sys.argv) > 2:
+        times = int(sys.argv[2])
+        print(f"The QLE will be executed {times} time\s.")
+
     for num in range(1, int(nParties)+1):
         label = ""
         while num > 0:
             num, remainder = divmod(num - 1, 26)
             label = chr(65 + remainder) + label
-        players.append(label)
-    print(players)
+            players[0].append(label)
+            players[1].append(0)
+    print(players[0])
 
-    start_time = time.time()
-    
-    w = quantumLeaderElection(players)
+    for i in range(times):
+        start_time = time.time()
 
-    finish_time = time.time()
+        w = quantumLeaderElection(players[0])
 
-    final_time = finish_time - start_time
-    print(f"The winner is {w}!")
-    print("--- Execution time: %s seconds ---" % final_time )
-    with open("test.txt", "a") as myfile:
-        myfile.write(f"{nParties} - log_p: {str(final_time)}\n")
+        finish_time = time.time()
+
+        index_w = players[0].index(w)
+        players[1][index_w] += 1
+
+        final_time = finish_time - start_time
+        mem_timings.append(final_time)
+
+        print(f"The winner is {w}!")
+        print("--- Execution time: %s seconds ---" % final_time )
+        with open("test.txt", "a") as myfile:
+            myfile.write(f"{nParties} - log_p: {str(final_time)}\n")
+
+if times>1:
+    probs = []
+    for i in range(len(players[0])):
+        prob = players[1][i]/times
+        probs.append(prob)
+    print(f"Nodes victory probabilities: {probs}.")
+
+    sum = 0
+    for i in range(times):
+        sum += mem_timings[i]
+    medium_time = sum/times
+    print(f"Medium execution time: {medium_time}.")
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for NODES in 64 66 75 100
+for NODES in 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 do
 echo "---------- NODES: $NODES ----------" >> test.txt
 python3 main_log_parallel.py $NODES
